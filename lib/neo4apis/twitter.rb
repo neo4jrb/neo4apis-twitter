be6801e15c2ff3cdafe0885f8fa39c167b78b7dd
Brian Underwood <ml+github@semi-sentient.com>
2014-11-06 11:22:32 +0100

    Adding supported fields

--- lib/neo4apis/twitter.rb
+++ lib/neo4apis/twitter.rb
@@ -15,7 +15,7 @@ module Neo4Apis
 
       retweeted_tweet_node = import :Tweet, tweet.retweeted_tweet if options[:import_retweets] && tweet.retweeted_tweet?
 
-      node = add_node :Tweet, tweet, %w{id text}
+      node = add_node :Tweet, tweet, %w{id text reply? geo urls media lang source created_at}
 
       if options[:import_hashtags] && tweet.respond_to?(:hashtags)
         tweet.hashtags.each do |hashtag|
@@ -33,7 +33,7 @@ module Neo4Apis
     end
 
     importer :User do |user|
-      add_node(:User, user, %w{id screen_name name location}) do |node|
+      add_node(:User, user, %w{id screen_name name description location profile_image_url created_at utc_offset time_zone lang followers_count verified? geo_enabled?}) do |node|
         node.profile_image_url = user.profile_image_url.to_s
       end
     end
