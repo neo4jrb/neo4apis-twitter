e3dee83a2cd9d28eab1086fdcb51e7d73348b798
Brian Underwood <ml+github@semi-sentient.com>
2015-03-27 22:17:07 +0100

    A lot of refactoring to support importing of friends and followers

--- lib/neo4apis/cli/twitter.rb
+++ lib/neo4apis/cli/twitter.rb
@@ -27,18 +27,95 @@ module Neo4Apis
       desc "search QUERY COUNT", "Import tweets via a search query"
       def search(query, count)
         neo4apis_client.batch do
-          twitter_client(false).search(query, options).take(count.to_i).each(&tweet_importer)
+          twitter_client.search(query, options).take(count.to_i).each(&tweet_importer)
+        end
+      end
+
+      desc "followers USERNAME DEPTH", "Imports followers (and optionally followers of followers if depth specified and with a value > 1"
+      def followers(user, depth = 1)
+        neo4apis_client.batch do
+          follower_importer(neo4apis_client, user, depth.to_i)
+        end
+      end
+
+      desc "friends USERNAME DEPTH", "Imports friends (and optionally friends of friends if depth specified and with a value > 1"
+      def friends(user, depth = 1)
+        neo4apis_client.batch do
+          friend_importer(neo4apis_client, user, depth.to_i)
         end
       end
 
       private
 
+      def follower_importer(neo4apis_client, user, depth)
+        friend_follower_importer(:followers, neo4apis_client, user, depth)
+      end
+
+      def friend_importer(neo4apis_client, user, depth)
+        friend_follower_importer(:friends, neo4apis_client, user, depth)
+      end
+
+      def friend_follower_importer(type, neo4apis_client, user, depth)
+        twitter_user = if user == ::Twitter::User
+                         user
+                       else
+                         throttled_block { twitter_client.user(user) }
+                       end
+        user_node = user_importer.call(twitter_user)
+
+        relationship = (type == :friends ? :friended : :follows)
+        get_friends_or_followers(type, twitter_user).each do |other_user|
+          other_user_node = user_importer.call(other_user)
+
+          neo4apis_client.add_relationship(relationship, other_user_node, user_node)
+
+          if depth > 1
+            friend_follower_importer(type, neo4apis_client, other_user, depth - 1)
+          end
+        end
+      end
+
+      def get_friends_or_followers(type, user)
+        [].tap do |user_list|
+          next_cursor = -1
+          while next_cursor != 0
+            result = throttled_block { twitter_client.send(type, user, cursor: next_cursor, count: 5000) }
+            throttled_block { user_list.concat(result.take(5000)) }
+            next_cursor = result.send(:next_cursor)
+          end
+        end
+      end
+
+      def throttled_block
+        yield
+      rescue ::Twitter::Error::TooManyRequests => error
+        puts error.inspect
+        puts error.rate_limit.inspect
+        reset_in = error.rate_limit.reset_in
+        puts "Rate limit exceeded.  Sleeping for #{reset_in} seconds"
+        sleep reset_in
+        retry
+      end
+
       def tweet_importer
+        @tweet_importer ||= object_importer(::Twitter::Tweet, :Tweet)
+      end
+
+      def user_importer
+        @user_importer ||= object_importer(::Twitter::User, :User)
+      end
+
+      def object_importer(gem_class, label)
         Proc.new do |object|
           case object
-          when ::Twitter::Tweet
-            say "got tweet from @#{object.user.screen_name.colorize(:light_blue)}: #{object.text}"
-            neo4apis_client.import :Tweet, object
+          when gem_class
+            case label
+            when :Tweet
+              say "got tweet from @#{object.user.screen_name.colorize(:light_blue)}: #{object.text}"
+            when :User
+              say "get user ##{object.id}"
+            end
+            neo4apis_client.import label, object
           when ::Twitter::Streaming::StallWarning
             say "FALLING BEHIND!".colorize(:red)
           end
@@ -48,10 +125,10 @@ module Neo4Apis
       NEO4APIS_CLIENT_CLASS = ::Neo4Apis::Twitter
 
       def neo4apis_client
-        @neo4apis_client ||= NEO4APIS_CLIENT_CLASS.new(Neo4j::Session.open(:server_db, parent_options[:neo4j_url]), import_retweets: options[:import_retweets], import_hashtags: options[:import_hashtags], import_user_mentions: options[:import_user_mentions])
+        @neo4apis_client ||= NEO4APIS_CLIENT_CLASS.new(neo4j_session, import_retweets: options[:import_retweets], import_hashtags: options[:import_hashtags], import_user_mentions: options[:import_user_mentions])
       end
 
-      def twitter_client(streaming)
+      def twitter_client(streaming = false)
         return @twitter_client if @twitter_client
 
         twitter_client_class = streaming ? ::Twitter::Streaming::Client : ::Twitter::REST::Client
